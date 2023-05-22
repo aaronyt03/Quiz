c90f1ff4b4b165c22b2cb95f593e637448439316
Aaron Lee <aaronyt03@gmail.com>
2023-05-22 16:56:14 +0800

    quiz setup

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/styles.css" />
+        <title>Pokemon</title>
+        
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1>Pokemon 1997</h1>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <h2>Gen 1 Knowledge</h2>
+
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+                Copyright &copy; 2023 Pokemon
+            </div>
+        </footer>
+    </body>
+</html>
